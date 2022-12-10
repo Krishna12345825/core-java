99a735af34dce970d8deb5546be90bed4fdae747
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-12-11 00:12:01 +0530

    LinkedList codes

---
+++ Collection-API/src/List/LunchArrayDeque1.java
@@ -0,0 +1,23 @@
+package List;
+
+import java.util.*;
+
+public class LunchArrayDeque1 
+{
+
+	public static void main(String[] args) 
+	{
+		ArrayDeque ad = new ArrayDeque();
+		ad.add(10);
+		ad.add(20);
+		ad.add(30);
+		ad.add(30);
+		System.out.println(ad);
+		
+		ad.addFirst(5);
+		ad.addLast(35);
+		System.out.println(ad);
+
+	}
+
+}
