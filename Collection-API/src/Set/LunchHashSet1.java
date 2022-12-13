bb9fe85f1ca62331685a599b00745f47360af250
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-12-13 19:31:10 +0530

    hashSet code

---
+++ Collection-API/src/Set/LunchHashSet1.java
@@ -0,0 +1,20 @@
+package Set;
+
+import java.util.*;
+
+public class LunchHashSet1 
+{
+
+	public static void main(String[] args)
+	{
+		HashSet hs = new HashSet();
+		hs.add(100);
+		hs.add(50);
+		hs.add(150);
+		hs.add(25);
+		hs.add(125);
+		System.out.println(hs);
+
+	}
+
+}
