c56de5ae53c2870214d6aa5328aa27512610fc9a
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-12-13 19:42:14 +0530

    treeset

--- Collection-API/src/Set/LunchTreeSet1.java
+++ Collection-API/src/Set/LunchTreeSet1.java
@@ -7,6 +7,7 @@ public class LunchTreeSet1
 	public static void main(String[] args) 
 	{
 		TreeSet  ts = new TreeSet();
+		
 		ts.add(100);
 		ts.add(50);
 		ts.add(150);
@@ -14,8 +15,17 @@ public class LunchTreeSet1
 		ts.add(75);
 		ts.add(125);
 		ts.add(175);
-		System.out.println();
+		System.out.println(ts);
+		System.out.println(ts.ceiling(50));
+		System.out.println(ts.higher(50));
+		System.out.println(ts.floor(50));
+		System.out.println(ts.lower(50));
+		System.out.println("******************************");
 		
+		System.out.println(ts.ceiling(40));
+		System.out.println(ts.higher(40));
+		System.out.println(ts.floor(40));
+		System.out.println(ts.lower(40));
 		
 	}
 
