35f013d58ab2849f57d4b6302d122a9e1a406d7f
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-12-13 19:37:03 +0530

    code

--- Collection-API/src/Set/LunchTreeSet1.java
+++ Collection-API/src/Set/LunchTreeSet1.java
@@ -1,10 +1,22 @@
 package Set;
 
-public class LunchTreeSet1 {
+import java.util.*;
+public class LunchTreeSet1
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		TreeSet  ts = new TreeSet();
+		ts.add(100);
+		ts.add(50);
+		ts.add(150);
+		ts.add(25);
+		ts.add(75);
+		ts.add(125);
+		ts.add(175);
+		System.out.println();
+		
+		
 	}
 
 }
