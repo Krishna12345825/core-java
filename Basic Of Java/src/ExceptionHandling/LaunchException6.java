fcb3b859c87e7ff91fa3a26dd2d4d89e7ebc3beb
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-12-15 00:12:26 +0530

    exception handlling

--- Basic Of Java/src/ExceptionHandling/LaunchException6.java
+++ Basic Of Java/src/ExceptionHandling/LaunchException6.java
@@ -1,15 +1,43 @@
 package ExceptionHandling;
 
+import java.util.*;
+
 //Re-throwing an exception
 class Alpha1
 {
-	
+	void alpha() throws ArithmeticException
+	{
+		System.out.println(" Connection to calculate app is established ");
+		try {
+			Scanner scan = new Scanner(System.in);
+			System.out.println(" Enter the first number to divide ");
+			int num1= scan.nextInt();
+			System.out.println(" Enter the Second number to divide ");
+			int num2= scan.nextInt();
+			int res= num1/num2;
+			System.out.println(" The result is "+ res);
+			
+		} catch (ArithmeticException  e) {
+			System.out.println(" Exception handled in alpha only ");
+			throw e;
+		}
+		finally {
+			System.out.println(" connection is terminated ");
+		}
+	}
 }
 
 public class LaunchException6 {
 
 	public static void main(String[] args) {
 		
+		try {
+			System.out.println(" Main method connetion ");
+			Alpha1 a = new Alpha1();
+			a.alpha();
+		} catch (ArithmeticException e) {
+			System.out.println(" Exception handled in main");
+		}
 
 	}
 
